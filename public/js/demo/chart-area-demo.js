5edc3441597a02fef092f773236b4c2175ad30d8
Bem Vindo! <caiofelipe-dev>
2025-12-02 21:06:39 -0300

    Adicionando diretórios e criando o arquivo template
    public/css/sb-admin-2.min.css

--- public/js/demo/chart-area-demo.js
+++
@@ -1,2 +0,0 @@
-// Placeholder demo script for chart area
-console && console.log && console.log('chart-area-demo placeholder loaded');
--- public/js/demo/chart-pie-demo.js
+++
@@ -1,2 +0,0 @@
-// Placeholder demo script for chart pie
-console && console.log && console.log('chart-pie-demo placeholder loaded');
